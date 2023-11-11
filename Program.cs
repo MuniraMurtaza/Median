c3eed96e45e4ed72152090611a1b1b5014dd6470
MuniraMurtaza <149403756+MuniraMurtaza@users.noreply.github.com>
2023-11-11 20:48:43 +0500

    jhas

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 namespace median {
     using System;
     using System.ComponentModel.Design;
@@ -9,31 +9,21 @@ namespace median {
         
         static void Main(string[] args)
         {
-            int[] nums1 = new int[] { 1, 2, 3, 4, 5, };
-            int[] nums2 = { 6, 7, 8, 9, 10 };
+            int[] nums1 = new int[] { 1, 2, 3 };
+            int[] nums2 = { 6, 7 , 8 };
 
             Console.WriteLine(FindMedianSortedArrays(nums1,nums2));
 
         }
          static double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-            double median = 0;
-            int[] num = new int[nums1.Length+nums2.Length];
-                
-            Array.Copy(nums1,num,nums1.Length);
-            Array.Copy(nums2,0, num, nums1.Length,nums2.Length);
-            if (nums1.Length != 0)
-            {
-                if (nums1.Length % 2 == 1)
-                {
-                    median = (num.Length + 1) / 2;
-
-                }
-                else
-                {
-                    median = ((num.Length / 2) + ((num.Length / 2) + 1)) / 2;
-
-                }
-            }
+           double median = 0.0;
+           nums1 = nums1.Concat(nums2).ToArray();
+           int arrayLength = nums1.Length;
+           
+           median = arrayLength == 0? 0 : (arrayLength % 2 == 1) ?Convert.ToDouble((arrayLength + 1) / 2): Convert.ToDouble((( arrayLength / 2) + ((arrayLength / 2) + 1)) / 2);
+           
+            
+            
             return median;
         }
 
